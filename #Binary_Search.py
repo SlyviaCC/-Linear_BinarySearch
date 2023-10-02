d28485ce856b7a650e8218db9f49498a61b205d4
shiwenjiao <slyviashi@didiglobal.com>
2023-10-02 16:38:56 +0800

    for binary test1

--- #Binary_Search.py
+++ #Binary_Search.py
@@ -1,38 +1,29 @@
-#Binary_Search
-import random
-list1 = []
-# Use a loop to generate a list of non-repeating elements
-for i in range(20):
-    #rand_num = random.randint(1,101)          
-    #list1.append(rand_num)                     
-    rand_num = random.randint(1,101)
-    while rand_num in list1:                 
-        rand_num = random.randint(1,101)
-    list1.append(rand_num)
+def binary_search(arr, x):
+    """
+    使用二分法在数组 arr 中查找 x
+    如果找到，返回 x 的索引，否则返回 -1
+    """
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        # 检查 x 是否存在于中间位置
+        if arr[mid] == x:
+            return mid
+        # 如果 x 大于中间位置的值，只需查找右边
+        elif arr[mid] < x:
+            left = mid + 1
+        # 否则只需查找左边
+        else:
+            right = mid - 1
+    # 如果我们到达这里，那么元素不在数组中
+    return -1
 
-#sortrows
-list1.sort()
-print(list1)
+# 示例
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+x = 5
+result = binary_search(arr, x)
 
-#find
-num = int(input("Enter the number what you want："))
-num_index = -1
-low = 0                 # The subscript of the minimum (lowest boundary)
-high = len(list1) - 1   #The subscript of the maximum value (maximum boundary)
-while high >= low:
-    mid = (low+high) // 2
-    #1.If the key is smaller than the middle element, just continue looking for the key in the first half of the list.
-    #2.If the key is equal to the intermediate element, the match is successful.  The search is complete.
-    #3.If the key is larger than the middle element, just continue to look for the key in the second half of the list.
-    if num<list1[mid]:
-        high = mid-1
-    elif num == list1[mid]:
-        num_index = mid
-        break
-    else:
-        low = mid + 1
-if num_index != -1:
-    print("{}the record the subscript of the number：{}.".format(num,num_index))
+if result != -1:
+    print(f"Element {x} is at index {result}")
 else:
-    print("The number you are looking for is not in the list！")
- 
+    print(f"Element {x} is not present in array")
